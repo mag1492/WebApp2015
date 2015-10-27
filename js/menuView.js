ca04447e095e621fbd96fb31107cc3db71ab2a07
Gabriel Simard <gabriel.simard.4@ulaval.ca>
2015-10-27 14:06:43 -0400

    menu dans une template

---
+++ js/menuView.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Gabriel on 2015-10-14.
+ */
+$(function() {
+
+    MenuView = Backbone.View.extend({
+        template : _.template($('#menu-template').html()),
+        el: '.menu',
+        render: function () {
+            this.$el.html(this.template());
+                }
+            });
+        });
