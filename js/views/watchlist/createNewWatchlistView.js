3f2cd3a47e54c4248f57a74ca17ec6afb536fc1b
Mag1492 <magaliecaouette@msn.com>
2015-11-04 23:34:11 -0500

    Gestion d'erreur si l'id d'une watchlist a modifiée est non trouvable.

--- js/views/watchlist/createNewWatchlistView.js
+++ js/views/watchlist/createNewWatchlistView.js
@@ -11,30 +11,35 @@ define([
         el: '.content',
 
         initialize:function(options){
-            this.optionsIdUndefined = 'false';
+            this.idIsUndefined = 'false';
+            this.watchlistOption= 'none';
+
             if(typeof options.id != 'undefined'){
+                this.watchlistOption = options.id.toString();
+                console.log(this.watchlistOption);
                 this.watchlist = new Watchlist({id: options.id});
             }
             else{
-                console.log("options is undefined");
-                this.optionsIdUndefined = 'true';
+                this.idIsUndefined = 'true';
                 this.watchlist = new Watchlist({id: null});
             }
         },
 
         render: function(){
             var that = this;
-            if(this.optionsIdUndefined === 'true'){
-                this.$el.html(this.template({watchlist: null}));
-            }
-            else{
+            if(this.idIsUndefined === 'false'){
                 that.watchlist.fetch({
                     success: function(response){
                         that.$el.html(that.template({watchlist: response.toJSON()}));
+                    },
+                    error: function(){
+                        alert("Error Not Found.\n The watchlist's id:" + that.watchlistOption + " was not found.");
                     }
                 });
             }
-
+            else{
+                this.$el.html(this.template({watchlist: null}));
+            }
         }
     });
 
