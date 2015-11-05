8f68194d315c986ab3d4b33bc8a8be88bdaa0d8f
Gabriel Simard <gabriel.simard.4@ulaval.ca>
2015-11-05 14:03:56 -0500

    add movies dans l'array yayayayayayyayay reste plus qu'a trouver pk i save pas

--- js/views/movie/movieWatchlistButtonView.js
+++ js/views/movie/movieWatchlistButtonView.js
@@ -7,9 +7,9 @@ define([
     'backbone',
     'text!templates/movie/movieWatchlistButtonTemplate.html',
     'models/watchlist',
-    'models/watchlistMovie',
+    'models/watchlistAddMovie',
     'models/movie'
-], function($, _, Backbone, MovieWatchlistButtonTemplate, Watchlist, WatchlistMovie, Movie){
+], function($, _, Backbone, MovieWatchlistButtonTemplate, Watchlist, WatchlistAddMovie, Movie){
     var MovieWatchlistButtonView = Backbone.View.extend({
         template : _.template(MovieWatchlistButtonTemplate),
         el: ".watchlist-button",
@@ -32,25 +32,19 @@ define([
 
         },
         addMovie: function(watchlistId, movieId){
-            var movieToAdd = new Movie({trackId : movieId});
-            var watchlistToModify;
-            movieToAdd.fetch({
-                success: function(response){
-                    var movies = [];
-                    movies.push(movieToAdd);
-                    watchlistToModify = new Watchlist({watchlistId : watchlistId, movies:movies});
-                    console.log(watchlistToModify);
-                    watchlistToModify.save({
+            var movie = new Movie({trackId : movieId});
+            movie.fetch({
+                success:function(response){
+                    var watchlist = new WatchlistAddMovie({watchlistId:watchlistId, movies:[response.toJSON()[0]]});
+                    console.log(watchlist);
+                    watchlist.save({
                         success:function(ret){
-                            console.log("sdfs");
-                            router.navigate('movie/'+movieId, {trigger: true});
+                            console.log("save");
                         }
-                    });
-
-
+                    })
                 }
-
             });
+
 
         }
     });
