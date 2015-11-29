588bf1e34444cd5e19b109b595e17ffa3c67cf6d
Mag1492 <magaliecaouette@msn.com>
2015-11-29 13:58:46 -0500

    Permet de prendre les gravatars pour tous followers, gere les followers empty et watchlist empty

--- js/views/user/userMainView.js
+++ js/views/user/userMainView.js
@@ -14,9 +14,9 @@ define([
         template : _.template(UserMainTemplate),
         el: '.content',
 
-        getGravatar: function(email) {
+        getGravatar: function(email, size) {
             var emailHash = $.md5(email);
-            var gravatarUrl = "http://www.gravatar.com/avatar/"+emailHash+"?s=200&d=http%3A%2F%2Fs21.postimg.org%2F68t2nogqv%2Flogo_Whitebg.jpg"
+            var gravatarUrl = "http://www.gravatar.com/avatar/"+emailHash+"?s="+size+"&d=http%3A%2F%2Fs21.postimg.org%2F68t2nogqv%2Flogo_Whitebg.jpg"
             return gravatarUrl;
         },
 
@@ -25,11 +25,16 @@ define([
             this.user.fetch({
                 success: function(response){
                     var user = response.toJSON();
-                    user.following.push(user);
                     that.$el.html(that.template({user: user}));
 
-                    var avatar = that.getGravatar(user.email);
-                    $("#avatar-img").css("background", "url("+ avatar+") center center no-repeat");
+                    var avatar = that.getGravatar(user.email, 200);
+                    $(".avatar-img").css("background", "url("+ avatar+") center center no-repeat");
+
+                    user.following.forEach( function(follower){
+                        var avatarFollower = that.getGravatar(follower.email, 100);
+                        $("#follower"+follower.id).find(".avatar-img-follower").css("background", "url("+ avatarFollower+") no-repeat");
+                    })
+
 
                     var view = new UserWatchlistView(user.id);
                     that.$el.append(view.render());
