cc44776031e75ae0880017780a1284002cb15dca
Gabriel Simard <gabriel.simard.4@ulaval.ca>
2015-11-29 13:29:05 -0500

    on peut chercher des episodes

--- js/views/tvshow/episodesView.js
+++ js/views/tvshow/episodesView.js
@@ -3,26 +3,45 @@ define([
     'underscore',
     'backbone',
     'text!templates/tvshow/SeasonEpisodeTemplate.html',
+    'views/tvshow/episodeListView',
     'collections/episodes'
-], function($, _, Backbone, SeasonEpisodeTemplate, Episodes){
+], function($, _, Backbone, SeasonEpisodeTemplate, EpisodeListView, Episodes){
     var EpisodesView = Backbone.View.extend({
         template : _.template(SeasonEpisodeTemplate),
         el: ".seasonX-episodes",
 
         initialize: function(id){
-            this.episodes = new Episodes({"seasonId" : id});
+            this.season = new Episodes({"seasonId" : id});
         },
 
         render: function(){
             var that = this;
-            this.episodes.fetch({
+            this.season.fetch({
                 success: function(response){
-                    var episodes = response.toJSON();
+                    that.episodes = response.toJSON();
                     that.$el.html(that.template({episodes: response.toJSON()}));
-                    var url = episodes[0].artworkUrl100.replace("100x100", "200x200");
+                    var url = that.episodes[0].artworkUrl100.replace("100x100", "200x200");
                     $("#cover").css("background", "url("+ url +") no-repeat");
+                    that.searchEpisode("");
+                    $('#search-episode').keypress(function() {
+                        var dInput = this.value;
+                        that.searchEpisode(dInput);
+                    });
                 }
             });
+        },
+        searchEpisode:function(searchTerms){
+            var view = new EpisodeListView();
+            var episodeResult = [];
+            if(searchTerms.trims != "") {
+                this.episodes.forEach(function (episode) {
+                    if (episode.trackName.toUpperCase().indexOf(searchTerms.toUpperCase()) > -1) {
+                        episodeResult.push(episode);
+                    }
+                });
+            }
+            this.$el.append(view.render(episodeResult));
+
         }
     });
     return EpisodesView;
