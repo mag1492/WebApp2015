c08256c920dc20be0e0be47cbf7b8a2d619fed3a
Gabriel Simard <gabriel.simard.4@ulaval.ca>
2015-11-30 14:19:21 -0500

    no need for this

--- js/views/search/userSearchView.js
+++ js/views/search/userSearchView.js
@@ -3,17 +3,14 @@ define([
     'underscore',
     'backbone',
     'text!templates/search/userResultTemplate.html',
-    'collections/searchResult/userResult',
-    'models/tokenInfo'
-], function($, _, Backbone, UserResultTemplate, UserResult, TokenInfo){
+    'collections/searchResult/userResult'
+], function($, _, Backbone, UserResultTemplate, UserResult){
     var userSearchView = Backbone.View.extend({
         template : _.template(UserResultTemplate),
         el: ".user-result",
 
         initialize: function(searchField){
             this.users = new UserResult({"searchField" : searchField});
-            this.tokenInfo = new TokenInfo();
-
         },
 
         render: function(){
