cf41eb73ab146078c83ca3750add4060bd80f507
Mag1492 <magaliecaouette@msn.com>
2015-12-03 21:34:06 -0500

    Verificaton pour log in fonctionnel!

--- js/views/signup/loginView.js
+++ js/views/signup/loginView.js
@@ -2,9 +2,10 @@ define([
     'jquery',
     'underscore',
     'backbone',
+    'sweetalarm',
     'text!templates/login/loginTemplate.html',
     'models/userLogin'
-], function($, _, Backbone, LoginTemplate, User){
+], function($, _, Backbone, swal, LoginTemplate, User){
     var LoginView = Backbone.View.extend({
         template : _.template(LoginTemplate),
 
@@ -12,7 +13,6 @@ define([
 
         render: function(){
             this.$el.html(this.template());
-
         },
 
         events: {
@@ -22,19 +22,36 @@ define([
         submit: function(e){
             e.preventDefault();
             var that = this;
-            var user = new User({email : $("#form-email").val(), password : $("#form-password").val()});
-            user.save(user.attributes, {
-                type: "POST",
-                contentType: "application/x-www-form-urlencoded",
-                data: $.param(user.attributes).toString(),
-                success: function(ret){
-                    goToHome();
-                }
-            });
+            var email = $("#form-email").val();
+            var password =$("#form-password").val();
+
+            if(!this.isEmailValid(email)){
+                swal("We're sorry...", "Your email is invalid. Try again.", "warning");
+            }else if(this.isPasswordEmpty(password)){
+                swal("We're sorry...", "Your password is empty. Try again.", "warning");
+            }else if(this.isEmailValid(email) && !this.isPasswordEmpty(password)){
+                var user = new User({email : email, password : password});
+                user.save(user.attributes, {
+                    type: "POST",
+                    contentType: "application/x-www-form-urlencoded",
+                    data: $.param(user.attributes).toString(),
+                    success: function(ret){
+                        goToHome();
+                    },
+                    error: function(ret){
+                        swal("We're sorry...", "Something went wrong. Try again.", "error");
+                    }
+                });
+            }
         },
 
         isEmailValid: function(email){
+            var regex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+            return regex.test(email);
+        },
 
+        isPasswordEmpty: function(password){
+           return password == "" || /^\s+$/.test(password);
         }
 
     });
