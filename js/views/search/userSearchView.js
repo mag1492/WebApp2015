940c45bb7b9cd4283738fca04ed27cf515f7cbac
Gabriel Simard <gabriel.simard.4@ulaval.ca>
2015-12-04 15:43:42 -0500

    follow dans search

--- js/views/search/userSearchView.js
+++ js/views/search/userSearchView.js
@@ -3,14 +3,16 @@ define([
     'underscore',
     'backbone',
     'text!templates/search/userResultTemplate.html',
+    'models/tokenInfo',
     'collections/searchResult/userResult'
-], function($, _, Backbone, UserResultTemplate, UserResult){
+], function($, _, Backbone, UserResultTemplate, TokenInfo, UserResult){
     var userSearchView = Backbone.View.extend({
         template : _.template(UserResultTemplate),
         el: ".user-result",
 
         initialize: function(searchField){
             this.users = new UserResult({"searchField" : searchField});
+            this.tokenInfo = new TokenInfo();
         },
 
         render: function(){
@@ -20,7 +22,30 @@ define([
                     xhr.setRequestHeader('Authorization', $.cookie('token'));
                 },
                 success: function(response) {
-                    that.$el.html(that.template({users: response.toJSON()}));
+                    var users = response.toJSON();
+                    that.tokenInfo.fetch({
+                        beforeSend: function (xhr) {
+                            xhr.setRequestHeader('Authorization', $.cookie('token'));
+                        },
+                        success: function (response) {
+                            var loggedUser = response.toJSON();
+                            users.forEach(function(user){
+                                user.isFollowing = false;
+                                if(user.name == loggedUser.name && user.email == loggedUser.email){
+                                    user.isFollowing = null;
+                                }
+                                loggedUser.following.forEach(function(follower){
+                                    if(user.name == follower.name && user.email == follower.email){
+                                        user.isFollowing = true;
+                                    }
+                                });
+
+                            });
+                            console.log(users);
+                            that.$el.html(that.template({users: users}));
+
+                        }
+                    });
                 }
             });
         }
