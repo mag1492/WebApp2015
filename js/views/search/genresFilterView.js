361284861aad611e40ee25e451b1bc72cebb00f0
brcc0 <brunoccoulombe@gmail.com>
2015-12-05 14:18:21 -0500

    console log

--- js/views/search/genresFilterView.js
+++ js/views/search/genresFilterView.js
@@ -16,11 +16,13 @@ define([
 
         render: function(){
             var that = this;
+            console.log(this.genres);
             this.genres.fetch({
                 beforeSend: function(xhr) {
                     xhr.setRequestHeader('Authorization', $.cookie('token'));
                 },
                 success: function(response){
+                    console.log("succes!");
                     that.$el.html(that.template({genres: response.toJSON()}));
                 }
             });
