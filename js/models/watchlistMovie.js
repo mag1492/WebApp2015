e1894cddcd1335e3476092bf5413cd1bd55c44b3
brcc0 <brunoccoulombe@gmail.com>
2015-12-06 11:01:21 -0500

    dropped

--- js/models/watchlistMovie.js
+++ js/models/watchlistMovie.js
@@ -3,7 +3,7 @@ define([
     'backbone'
 ], function(_, Backbone) {
     var WatchlistMovie = Backbone.Model.extend({
-        url : 'http://umovie.herokuapp.com/unsecure/watchlists/563925ff634eb80300b1f151',
+        url : 'http://umovie.herokuapp.com/watchlists/563925ff634eb80300b1f151',
 
         initialize: function (options) {
             if (options.watchlistId) {
