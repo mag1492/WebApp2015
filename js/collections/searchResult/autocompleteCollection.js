2016a7578c98311a35c1b03dbc98270e30d25c0e
camillebeland <camillebeland1@gmail.com>
2015-12-13 20:53:13 -0500

    inception is better

--- js/collections/searchResult/autocompleteCollection.js
+++ js/collections/searchResult/autocompleteCollection.js
@@ -5,7 +5,7 @@ define([
 ], function($, _, Backbone){
     var AutocompleteCollection = Backbone.Collection.extend({
         initialize: function(text){
-                this.url = 'https://itunes.apple.com/search?term=' + text + '&media=movie&limit=10';
+                this.url = 'https://itunes.apple.com/search?term=' + text + '&media=movie&entity=movie&attribute=movieTerm&limit=10';
         },
         parse: function(response) {
             return response.results;
